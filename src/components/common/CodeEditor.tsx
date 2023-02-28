169df668932a3f293d3985d40d7bd40c99d87d31
xueyong <1085523029@qq.com>
2023-02-28 17:57:57 +0800

    🔥 reduce monaco file size

--- src/components/common/CodeEditor.tsx
+++ src/components/common/CodeEditor.tsx
@@ -1,24 +1,44 @@
 import MonacoEditor, { BeforeMount, EditorProps, Monaco, loader } from "@monaco-editor/react"
 
-import * as monaco from "monaco-editor"
+import "monaco-editor/esm/vs/editor/editor.all.js"
+
+import "monaco-editor/esm/vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp.js"
+import "monaco-editor/esm/vs/editor/standalone/browser/iPadShowKeyboard/iPadShowKeyboard.js"
+import "monaco-editor/esm/vs/editor/standalone/browser/inspectTokens/inspectTokens.js"
+import "monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneHelpQuickAccess.js"
+import "monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess.js"
+import "monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoSymbolQuickAccess.js"
+import "monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess.js"
+import "monaco-editor/esm/vs/editor/standalone/browser/referenceSearch/standaloneReferenceSearch.js"
+
+import "monaco-editor/esm/vs/language/typescript/monaco.contribution"
+// import 'monaco-editor/esm/vs/language/css/monaco.contribution';
+// import 'monaco-editor/esm/vs/language/json/monaco.contribution';
+// import 'monaco-editor/esm/vs/language/html/monaco.contribution';
+// import 'monaco-editor/esm/vs/basic-languages/monaco.contribution';
+import "monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution.js"
+import "monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution.js"
+
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api"
+
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker"
-import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker"
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker"
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker"
+// import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker"
+// import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker"
+// import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker"
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker"
 import { NAMESPACE_TYPE } from "@/constants/codeEditor"
 
 self.MonacoEnvironment = {
   getWorker(_, label) {
-    if (label === "json") {
-      return new jsonWorker()
-    }
-    if (label === "css" || label === "scss" || label === "less") {
-      return new cssWorker()
-    }
-    if (label === "html" || label === "handlebars" || label === "razor") {
-      return new htmlWorker()
-    }
+    // if (label === "json") {
+    //   return new jsonWorker()
+    // }
+    // if (label === "css" || label === "scss" || label === "less") {
+    //   return new cssWorker()
+    // }
+    // if (label === "html" || label === "handlebars" || label === "razor") {
+    //   return new htmlWorker()
+    // }
     if (label === "typescript" || label === "javascript") {
       return new tsWorker()
     }
